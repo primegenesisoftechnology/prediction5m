89ab49c83f8f8d3a360d8389bbb978ba90d4c694
Ahmad Malik <121057088+Malik-AhmadSE@users.noreply.github.com>
2023-12-31 00:00:51 +0500

    change

---
+++ app.py
@@ -0,0 +1,52 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+import threading
+import time
+import websocket
+import json
+import pickle
+import numpy as np
+import gzip
+app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origins="*")
+kline_data_5m=None
+model_5m = pickle.load(gzip.open('model_gzip_5m.pkl', 'rb'))
+@socketio.on('connect')
+def Live_stream():        
+    def Five_Minute_Function():
+        symbol = "btcusdt" 
+        interval ="5m"
+        def on_message(ws_app, message):
+            global kline_data_5m
+            data = json.loads(message)
+            open_val = float(f"{float(data['k']['o']): .2f}")
+            high_val = float(f"{float(data['k']['h']): .2f}")
+            low_val = float(f"{float(data['k']['l']): .2f}")
+            volume_val = float(f"{float(data['k']['v']): .2f}")
+            values = [open_val, high_val, low_val, volume_val]
+            arr = [np.array(values)]
+            prediction_5m=model_5m.predict(arr)  
+            kline_data_5m={"predict": prediction_5m.tolist()}
+            ws_app.close()
+        def on_error(_, error):
+            print(f"WebSocket Error: {error}")
+        def on_close(_, close_status_code, close_msg):
+            print("WebSocket Closed")
+        def on_open(ws_app):
+            print('WebSocket opened')
+        websocket_url = 'wss://stream.binance.com:9443/ws/' + symbol + '@kline_' + interval
+        ws_app = websocket.WebSocketApp(websocket_url, on_open=on_open, on_message=on_message, on_error=on_error, on_close=on_close)
+        ws_app.run_forever()
+        while True:
+            socketio.emit('data_5m', {'message': kline_data_5m})
+            # time.sleep(1)
+   
+    Five_Minute_Thread = threading.Thread(target=Five_Minute_Function)
+    Five_Minute_Thread.start()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    socketio.run(app, debug=True)
---
+++ models_train.py
@@ -0,0 +1,41 @@
+from sklearn import linear_model
+import pickle
+import numpy as np
+import pandas as pd
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.pipeline import make_pipeline, make_union
+from sklearn.preprocessing import MaxAbsScaler
+from sklearn.ensemble import RandomForestRegressor
+# from tpot.builtins import StackingEstimator
+from sklearn import linear_model
+from sklearn.kernel_approximation import Nystroem
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+import warnings
+import gzip
+from xgboost import XGBRegressor
+from xgboost import XGBRFRegressor
+
+df = pd.read_csv("BTCDATA.csv")
+warnings.filterwarnings("ignore", category=UserWarning)
+# take a look at the dataset
+#df.head()
+#use required features
+cdf = df[['open','high','low','volume','close']]
+#Training Data and Predictor Variable
+# Use all data for training (tarin-test-split not used)
+x=cdf.iloc[:,:4]
+print(x)
+y = cdf.iloc[:, -1]
+print(y)
+reg=XGBRegressor()
+
+#Fitting model with trainig data
+reg.fit(x, y)
+
+# Saving model to disk
+# Pickle serializes objects so they can be saved to a file, and loaded in a program again later on.
+# joblib.dump(reg, 'Model_5m.pkl')
+pickle.dump(reg, gzip.open('model_gzip_1m.pkl', 'wb'))
+#model = pickle.load(open('model.pkl','rb'))
+#print(model.predict([[2.6, 8, 10.1]]))
